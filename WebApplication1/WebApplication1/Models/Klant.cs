1cb61f88f2c162399eaec36bc7bcfe352dd39c0a
JimButseraen <jim.butseraen@gmail.com>
2015-05-29 11:40:52 +0200

    Basis voor Models

---
+++ WebApplication1/WebApplication1/Models/Klant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Klant
+    {
+        public int ID { get; set; }
+        public string Naam { get; set; }
+        public string Voornaam { get; set; }
+        public Adres Adres { get; set; }
+        public string Telefoon { get; set; }
+        public string Email { get; set; }
+        public Geslacht Geslacht { get; set; }
+        public DateTime Geboortedatum { get; set; }
+        public Mutualiteit Mutualiteit { get; set; }
+        public List<Karaktertrek> Karaktertrek { get; set; }
+        public string Hobby { get; set; }
+        public Werksituatie Werksituatie { get; set; }
+        public string Extra { get; set; }
+        public string Allergie { get; set; }
+        public string Blessure { get; set; }
+        public string AndereMedische { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Naam { get; set; }
+        public Decimal Prijs { get; set; }
+        public int Inhoud { get; set; }
+        public String Foto { get; set; }
+    }
+}
