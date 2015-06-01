ee1a02c0448ccaaefe9b81e602ba87e64e35de3a
JimButseraen <jim.butseraen@gmail.com>
2015-06-01 11:45:39 +0200

    Data opvullen

--- WebApplication1/WebApplication1/Migrations/Configuration.cs
+++ WebApplication1/WebApplication1/Migrations/Configuration.cs
@@ -4,6 +4,7 @@ namespace WebApplication1.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using WebApplication1.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WebApplication1.Models.ApplicationDbContext>
     {
@@ -14,18 +15,38 @@ namespace WebApplication1.Migrations
 
         protected override void Seed(WebApplication1.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            //GESLACHT
+            context.Geslacht.AddOrUpdate<Geslacht>(g => g.Naam, new Geslacht() { Naam = "M" });
+            context.Geslacht.AddOrUpdate<Geslacht>(g => g.Naam, new Geslacht() { Naam = "V" });
+            context.Geslacht.AddOrUpdate<Geslacht>(g => g.Naam, new Geslacht() { Naam = "X" });
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            //MUTUALITEIT
+            context.Mutualiteit.AddOrUpdate<Mutualiteit>(m => m.Naam, new Mutualiteit() { Naam = "Christelijke Mutualiteit" });
+            context.Mutualiteit.AddOrUpdate<Mutualiteit>(m => m.Naam, new Mutualiteit() { Naam = "Socialistische Mutualiteit" });
+            context.Mutualiteit.AddOrUpdate<Mutualiteit>(m => m.Naam, new Mutualiteit() { Naam = "Liberale Mutualiteit" });
+            context.Mutualiteit.AddOrUpdate<Mutualiteit>(m => m.Naam, new Mutualiteit() { Naam = "OZ Onafhankelijk Ziekenfonds" });
+            context.Mutualiteit.AddOrUpdate<Mutualiteit>(m => m.Naam, new Mutualiteit() { Naam = "Onafhankelijk Ziekendfonds Securex" });
+            context.Mutualiteit.AddOrUpdate<Mutualiteit>(m => m.Naam, new Mutualiteit() { Naam = "Partena Onafhankelijk Ziekenfonds" });
+            context.Mutualiteit.AddOrUpdate<Mutualiteit>(m => m.Naam, new Mutualiteit() { Naam = "Vlaams & Neutraal Ziekenfonds" });
+            context.Mutualiteit.AddOrUpdate<Mutualiteit>(m => m.Naam, new Mutualiteit() { Naam = "Symbio" });
+            context.Mutualiteit.AddOrUpdate<Mutualiteit>(m => m.Naam, new Mutualiteit() { Naam = "Neutraal Ziekenfonds Vlaanderen" });
+
+            //SOORT AFSPRAAK
+            context.SoortAfspraak.AddOrUpdate<SoortAfspraak>(s => s.Naam, new SoortAfspraak() { Naam = "Massage met etherische olie", Omschrijving = "De massage duurt 60 tot 75 minuten, discreet uitgevoerd met olie op de blote huid, op een massagetafel.", Duur = 60, Prijs = 50, Verplaatsingmogelijk=true });
+            context.SoortAfspraak.AddOrUpdate<SoortAfspraak>(s => s.Naam, new SoortAfspraak() { Naam = "Kaarsmassage", Omschrijving = "De ontspannende kaarsmassage is een relaxerende gehele lichaamsmassage uitgevoerd met een kaars, gemaakt van een gestolde biologische etherische oliënolie in een rustige, aangenaam verwarmde ruimte.", Duur = 60, Prijs = 60, Verplaatsingmogelijk = true });
+            context.SoortAfspraak.AddOrUpdate<SoortAfspraak>(s => s.Naam, new SoortAfspraak() { Naam = "Japanse gezichtsmassage", Omschrijving = "De massage duurt 60 minuten met gebruik van hoogwaardige producten van biologische kwaliteit van Ahava.", Duur = 60, Prijs = 50, Verplaatsingmogelijk = true });
+            context.SoortAfspraak.AddOrUpdate<SoortAfspraak>(s => s.Naam, new SoortAfspraak() { Naam = "Fibromyssage", Omschrijving = "Wij starten altijd met een intake gesprek. Hierna zullen we jou ’plaatselijk behandelen’ én/of masseren. Hierbij gaan we samen met jou op zoek naar waar je pijn vandaan komt en/of zich situeert, en tegelijk zoeken we samen naar je pijngrens.", Duur = 30, Prijs = 30, Verplaatsingmogelijk = true });
+            context.SoortAfspraak.AddOrUpdate<SoortAfspraak>(s => s.Naam, new SoortAfspraak() { Naam = "Shiatsu", Omschrijving = "Shiatsu is een massagevorm uit Japan, letterlijk vertaald ‘Vingerdruk’ en komt voort uit een lange traditie van de Oosterse Natuurlijke Geneeswijzen. Deze drukpuntmassage werkt in op de stroom van de meridianen of energiebanen in het lichaam, stimuleert de zelfhelende kracht van het lichaam en helpt om terug gezond te worden en te blijven.", Duur = 60, Prijs = 50, Verplaatsingmogelijk = true });
+            context.SoortAfspraak.AddOrUpdate<SoortAfspraak>(s => s.Naam, new SoortAfspraak() { Naam = "Lomi-Lomi massage", Omschrijving = "De Lomi-Lomi massage of Hawaïaanse lichaamsmassage heeft tot doel je energiesysteem in balans te brengen. Het is een krachtige, innemende massage, vloeiend en ritmisch tegelijk als de dansende golven op de zwarte zandstranden, soms zacht en soms krachtig.", Duur = 60, Prijs = 60, Verplaatsingmogelijk = true });
+            
+            //WERKSITUATIE
+            context.Werksituatie.AddOrUpdate<Werksituatie>(w => w.Naam, new Werksituatie() { Naam = "Leerling" });
+            context.Werksituatie.AddOrUpdate<Werksituatie>(w => w.Naam, new Werksituatie() { Naam = "Student" });
+            context.Werksituatie.AddOrUpdate<Werksituatie>(w => w.Naam, new Werksituatie() { Naam = "Arbeider" });
+            context.Werksituatie.AddOrUpdate<Werksituatie>(w => w.Naam, new Werksituatie() { Naam = "Bediende" });
+            context.Werksituatie.AddOrUpdate<Werksituatie>(w => w.Naam, new Werksituatie() { Naam = "Gepensioneerd" });
+            context.Werksituatie.AddOrUpdate<Werksituatie>(w => w.Naam, new Werksituatie() { Naam = "Werkzoekend" });
+
         }
     }
 }
