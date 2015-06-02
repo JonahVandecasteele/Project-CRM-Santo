0ca65e1d9e9acffa1fbc4b09b8b605da87b7891c
Iceman344 <iceman344@gmail.com>
2015-06-02 13:41:21 +0200

    Unity Setup + Services

---
+++ CRMSanto/CRMSanto.BusinessLayer/Services/KlantService.cs
@@ -0,0 +1,31 @@
+﻿using CRMSanto.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMSanto.BusinessLayer.Services
+{
+    class KlantService
+    {
+        public List<Klant> GetKlanten()
+        {
+            return new List<Klant>();
+        }
+        public Klant GetKlantByID(int id)
+        {
+            return new Klant();
+        }
+        public List<Klant> GetKlantenByPostCode(Adres Add)
+        {
+            return new List<Klant>();
+        }
+        public Klant AddKlant(Klant klant,Stream Image)
+        {
+            StorageHelper.AddImage("WebShop.Properties.Settings.StorageConnectionString", "images", Image, System.Guid.NewGuid().ToString());
+            return klant;
+        }
+    }
+}
