fb22a75eb430aa96b63722ae4f77b42b991beb4f
JimButseraen <jim.butseraen@gmail.com>
2015-06-08 13:43:43 +0200

    Feest

--- CRMSanto/CRMSanto.BusinessLayer/Repository/KlantenRepository.cs
+++ CRMSanto/CRMSanto.BusinessLayer/Repository/KlantenRepository.cs
@@ -11,7 +11,8 @@ namespace CRMSanto.BusinessLayer.Repository
 {
     public class KlantenRepository : GenericRepository<Klant>, IKlantenRepository
     {
-        public KlantenRepository(ApplicationDbContext context) : base(context)
+        public KlantenRepository(ApplicationDbContext context)
+            : base(context)
         {
 
         }
@@ -22,7 +23,7 @@ namespace CRMSanto.BusinessLayer.Repository
         }
         public override Klant GetByID(object id)
         {
-            var query = (from k in context.Klant.Include(g => g.Geslacht).Include(kar => kar.Karaktertrek).Include(m => m.MedischeFiche).Include(p => p.PersoonlijkeFiche) where k.ID==(int)id select k);
+            var query = (from k in context.Klant.Include(g => g.Geslacht).Include(kar => kar.Karaktertrek).Include(m => m.MedischeFiche).Include(p => p.PersoonlijkeFiche) where k.ID == (int)id select k);
             return query.SingleOrDefault<Klant>();
         }
         public IEnumerable<Klant> GetByPostCode(string postcode)
@@ -39,7 +40,7 @@ namespace CRMSanto.BusinessLayer.Repository
                 context.Karaktertrek.Attach(kar);
             }
             context.MedischeFiche.Add(entity.MedischeFiche);
-            context.PersoonlijkeFiche.Add(entity.PersoonlijkeFiche);            
+            context.PersoonlijkeFiche.Add(entity.PersoonlijkeFiche);
             Klant klant = context.Klant.Add(entity);
             return klant;
         }
