bcfa69b341113aaa812c25ac2d0fac12b7abb8b5
Iceman344 <iceman344@gmail.com>
2015-06-08 14:28:05 +0200

    Klanten (No double posts)

---
+++ CRMSanto/CRMSanto.BusinessLayer/Repository/IKaraktertrekRepository.cs
@@ -0,0 +1,9 @@
+﻿using CRMSanto.Models;
+using System;
+namespace CRMSanto.BusinessLayer.Repository
+{
+    public interface IKaraktertrekRepository : IGenericRepository<Karaktertrek>
+    {
+        System.Collections.Generic.IEnumerable<CRMSanto.Models.Karaktertrek> All();
+    }
+}
---
+++ CRMSanto/CRMSanto.BusinessLayer/Repository/KaraktertrekRepository.cs
@@ -0,0 +1,31 @@
+﻿using CRMSanto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace CRMSanto.BusinessLayer.Repository
+{
+    public class KaraktertrekRepository : GenericRepository<Karaktertrek>, CRMSanto.BusinessLayer.Repository.IKaraktertrekRepository
+    {
+        public KaraktertrekRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+        public override IEnumerable<Karaktertrek> All()
+        {
+            var query = (from k in context.Karaktertrek.Include(k => k.Klanten) select k);
+            return query.ToList<Karaktertrek>();
+        }
+        public override Karaktertrek GetByID(object id)
+        {
+            var query = (from k in context.Karaktertrek.Include(k => k.Klanten) where k.ID==(int)id select k);
+            return query.Single<Karaktertrek>();
+        }
+
+    }
+}
