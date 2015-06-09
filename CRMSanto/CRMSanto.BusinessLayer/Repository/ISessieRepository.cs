9de39d9a2dd3c14ed7e7a5f6c8d8a3259d15e8c9
Iceman344 <iceman344@gmail.com>
2015-06-09 15:46:56 +0200

    Sessies

---
+++ CRMSanto/CRMSanto.BusinessLayer/Repository/ISessieRepository.cs
@@ -0,0 +1,11 @@
+﻿using CRMSanto.Models;
+using System;
+namespace CRMSanto.BusinessLayer.Repository
+{
+    public interface ISessieRepository : IGenericRepository<Sessie>
+    {
+        CRMSanto.Models.Sessie GetByKlantID(object id);
+        CRMSanto.Models.Sessie Insert(CRMSanto.Models.Sessie entity);
+        void Update(CRMSanto.Models.Sessie entityToUpdate);
+    }
+}
