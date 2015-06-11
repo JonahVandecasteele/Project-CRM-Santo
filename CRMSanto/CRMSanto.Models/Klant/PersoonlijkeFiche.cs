c5e4515506ff4eed290fac3c5d38625de8cb34c1
JimButseraen <jim.butseraen@gmail.com>
2015-06-11 14:40:53 +0200

    nog 4 remaining

--- CRMSanto/CRMSanto.Models/Klant/PersoonlijkeFiche.cs
+++ CRMSanto/CRMSanto.Models/Klant/PersoonlijkeFiche.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CRMSanto.Models.Klant;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,7 @@ namespace CRMSanto.Models
     {
         public int ID { get; set; }
         public string Hobby { get; set; }
-        public string Werksituatie { get; set; }
+        public Werksituatie Werksituatie { get; set; }
         public string Gezinssituatie { get; set; }
     }
 }
---
+++ CRMSanto/CRMSanto.Models/Klant/Werksituatie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMSanto.Models.Klant
+{
+    public class Werksituatie
+    {
+        public int ID { get; set; }
+        public string Naam { get; set; }
+    }
+}
