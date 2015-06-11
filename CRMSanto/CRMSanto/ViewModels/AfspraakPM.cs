62aec4661e9ba95eeb0777d039a585a24a9edef7
Hannes Declercq <hannes.declercq@gmail.com>
2015-06-11 10:50:50 +0200

    Afgrijselijke kalender

---
+++ CRMSanto/CRMSanto/ViewModels/AfspraakPM.cs
@@ -0,0 +1,15 @@
+﻿using CRMSanto.Calendar;
+using CRMSanto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRMSanto.ViewModels
+{
+    public class AfspraakPM
+    {
+        public List<Afspraak> Afspraken;
+        public WeekForMonth Kalender;
+    }
+}
