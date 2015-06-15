ba7cef438c0b3c9a40c10806c2a20f560ceacc34
JimButseraen <jim.butseraen@gmail.com>
2015-06-15 11:20:16 +0200

    webjob nog niet in orde

--- CRMSanto/VerjaardagWebJob/Program.cs
+++ CRMSanto/VerjaardagWebJob/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using CRMSanto.Models;
 using CRMSanto.BusinessLayer.Repository;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage;
+using System.Configuration;
 
 namespace VerjaardagWebJob
 {
@@ -16,9 +19,21 @@ namespace VerjaardagWebJob
         // AzureWebJobsDashboard and AzureWebJobsStorage
         static void Main()
         {
+            Jarigen();
             var host = new JobHost();
             // The following code ensures that the WebJob will be running continuously
             host.RunAndBlock();
+        }
+
+        public static void Jarigen()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            KlantenRepository kr = new KlantenRepository(context);
+            List<Klant> jarigen = kr.GetJarigen();
+            foreach(Klant k in jarigen)
+            {
+                kr.SendMail(k);
+            }
         }
     }
 }
