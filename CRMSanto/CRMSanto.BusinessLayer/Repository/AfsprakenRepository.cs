52945e22f97220adb047082916ea217bc7ade851
Iceman344 <iceman344@gmail.com>
2015-06-18 21:14:07 +0200

    Update Done

--- CRMSanto/CRMSanto.BusinessLayer/Repository/AfsprakenRepository.cs
+++ CRMSanto/CRMSanto.BusinessLayer/Repository/AfsprakenRepository.cs
@@ -111,35 +111,48 @@ namespace CRMSanto.BusinessLayer.Repository
         public override void Update(Afspraak entityToUpdate)
         {
             Afspraak old = GetByID(entityToUpdate.ID);
-            if (old.Klant != null && old.Klant.ID != entityToUpdate.Klant.ID)
+           /* if (old.Klant.Adres == null)
+                old.Klant.Adres = new Adres();
+            if (old.Klant.Adres.Gemeente == null)
+                old.Klant.Adres.Gemeente = new Gemeente();*/
+            if (old.SoortAfspraak == null)
+                old.SoortAfspraak = new SoortAfspraak();
+            if (old.Masseur == null)
+                old.Masseur = new Masseur();
+            if (old.Arrangement == null)
+                old.Arrangement = new Arrangement();
+            if (old.Extra == null)
+                old.Extra = new Extra();
+
+            if (old.Klant.ID != entityToUpdate.Klant.ID)
             {
                 context.Klant.Attach(entityToUpdate.Klant);
                 context.Afspraak.Include(m => m.Klant).FirstOrDefault(m => m.ID == entityToUpdate.ID).Klant = entityToUpdate.Klant;
             }
-            if (old.SoortAfspraak != null && old.SoortAfspraak.ID != entityToUpdate.SoortAfspraak.ID)
+            if (old.SoortAfspraak.ID != entityToUpdate.SoortAfspraak.ID)
             {
                 context.SoortAfspraak.Attach(entityToUpdate.SoortAfspraak);
                 context.Afspraak.Include(m => m.SoortAfspraak).FirstOrDefault(m => m.ID == entityToUpdate.ID).SoortAfspraak = entityToUpdate.SoortAfspraak;
             }
-            context.Entry(old.Adres).CurrentValues.SetValues(entityToUpdate.Adres);
-            if (old.Adres.Gemeente != null && old.Adres.Gemeente.ID != entityToUpdate.Adres.Gemeente.ID && entityToUpdate.Adres.Gemeente.ID != 0)
+          /*  context.Entry(old.Klant.Adres).CurrentValues.SetValues(entityToUpdate.Klant.Adres);
+            if (old.Klant.Adres.Gemeente.ID != entityToUpdate.Klant.Adres.Gemeente.ID && entityToUpdate.Klant.Adres.Gemeente.ID != 0)
             {
-                context.Gemeente.Attach(entityToUpdate.Adres.Gemeente);
-                context.Adres.Include(m => m.Gemeente).FirstOrDefault(m => m.ID == entityToUpdate.Adres.ID).Gemeente = entityToUpdate.Adres.Gemeente;
-            }
+                context.Gemeente.Attach(entityToUpdate.Klant.Adres.Gemeente);
+                context.Adres.Include(m => m.Gemeente).FirstOrDefault(m => m.ID == entityToUpdate.Klant.Adres.ID).Gemeente = entityToUpdate.Klant.Adres.Gemeente;
+            }*/
 
-            if (old.Masseur != null && old.Masseur.ID != entityToUpdate.Masseur.ID)
+            if (old.Masseur.ID != entityToUpdate.Masseur.ID)
             {
                 context.Masseur.Attach(entityToUpdate.Masseur);
                 context.Afspraak.Include(m => m.Masseur).FirstOrDefault(m => m.ID == entityToUpdate.ID).Masseur = entityToUpdate.Masseur;
             }
 
-            if (old.Arrangement != null && old.Arrangement.ID != entityToUpdate.Arrangement.ID)
+            if (old.Arrangement.ID != entityToUpdate.Arrangement.ID)
             {
                 context.Arrangement.Attach(entityToUpdate.Arrangement);
                 context.Afspraak.Include(m => m.Arrangement).FirstOrDefault(m => m.ID == entityToUpdate.ID).Arrangement = entityToUpdate.Arrangement;
             }
-            if (old.Extra != null && old.Extra.ID != entityToUpdate.Extra.ID)
+            if (old.Extra.ID != entityToUpdate.Extra.ID)
             {
                 context.Extra.Attach(entityToUpdate.Extra);
                 context.Afspraak.Include(m => m.Extra).FirstOrDefault(m => m.ID == entityToUpdate.ID).Extra = entityToUpdate.Extra;
